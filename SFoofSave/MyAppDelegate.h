4d262768753af1275551d59ac886c1e17fccf83f
LiuFeiFei <851085835@qq.com>
2016-08-04 10:26:16 +0800

    33

---
+++ SFoofSave/MyAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFoofSave
+//
+//  Created by Lff on 16/8/3.
+//  Copyright © 2016年 Lff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SFoofSave/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SFoofSave
+//
+//  Created by Lff on 16/8/3.
+//  Copyright © 2016年 Lff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MyAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyAppDelegate class]));
+    }
+}
